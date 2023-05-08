a6daaae13f8518a89cd40f505ba0522161dc92fc
EllyBB <ekbayonab@gmail.com>
2023-05-08 13:55:05 -0600

    Pointers

---
+++ Ejercicios 7 Pointers/Ninja_Level7_2.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+
+type person struct{
+    name string
+}
+
+func changeMe(p *person, other_name string){
+    p.name=other_name
+    
+    //to dereference a struct, use (*value).field
+    // (*p).name = other_name
+}
+
+
+func main(){
+    p1 := person{"Elly"}
+    fmt.Println(p1.name)
+    
+    // Sin los apuntadores esto no funcionaría!! 
+    changeMe(&p1,"Anne")
+    fmt.Println(p1.name)
+    
+    
+}
